64b5f8fd0e5d0fd4b2dc442f8f8979cdba888ef2
Bitaniyagetu <bituzelalem@gmail.com>
2025-07-26 17:03:03 -0400

    cleaned up CSS for homepage

--- public/script.js
+++ public/script.js
@@ -1,1 +1,16 @@
 console.log("Script loaded");
+// script.js
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+  let reveals = document.querySelectorAll('.menu-card');
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 100;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add('active');
+    }
+  }
+}
